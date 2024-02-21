7fc002cdd7124d9340e48b5eed9c2214713d65de
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-02-21 15:24:56 +0100

    added form

--- index.php
+++ index.php
@@ -7,9 +7,29 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!--BOOTSTRAP-->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Document</title>
 </head>
-<body>
+<body class="bg-secondary">
+    <div class="container">
+        <header class="text-center my-5">
+            <h1>Strong Password Generator</h1>
+            <h2 class="text-white">Genera una password sicura</h2>
+        </header>
+        <main>
+            <div class="container bg-warning">
+                <form action="" method="GET">
+                    <div class="mb-3 d-flex align-items-center justify-content-between">
+                        <label for="password" class="form-label">Lunghezza Password:</label>
+                        <input type="password" class="form-control w-50">
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </main>
+    </div>
+    
     
 </body>
 </html>
